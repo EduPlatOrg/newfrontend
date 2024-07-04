db61dc29fe213c150ab1194a20a47533787f2733
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-07-04 18:18:36 +0200

    add examples to EventDetailCard

--- src/components/EventDetailCard.jsx
+++ src/components/EventDetailCard.jsx
@@ -1,7 +1,21 @@
+import ReactPlayer from 'react-player';
+
 const EventDetailCard = ({ event }) => {
   //TODO: AQUI PUEDES MAQUETAR EL EVENTO COMPLETO CON TODOS SUS DATOS, HAY QUE DEJAR UN BOTON A LA PAGINA DE INSCRIPCIONES Y OTRO PARA VOLVER ATRAS.
   console.log(event);
-  return <div>{event._id}</div>;
+  return (
+    <div>
+      <p>{event._id}</p>
+      <iframe
+        src={event.pdfDocument}
+        frameBorder='0'></iframe>
+
+      <ReactPlayer
+        url={event.youtubeUrl}
+        controls={true}
+      />
+    </div>
+  );
 };
 
 export default EventDetailCard;
