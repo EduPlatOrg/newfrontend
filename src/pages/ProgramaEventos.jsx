fd6f929e48f1d8f58236ac23f782b786fac32754
Alejandro Vives <alejandrovt1996@gmail.com>
2024-07-06 16:05:50 +0200

    past y next ev

--- src/pages/ProgramaEventos.jsx
+++ src/pages/ProgramaEventos.jsx
@@ -31,49 +31,13 @@ const ProgramaEventos = () => {
         <NavbarLinks />
       </div>
 
-      {/* pastEvents */}
-      <div className='flex flex-col gap-4 mt-4'>
-        {pastEvents.map((event) => (
-          <div
-            key={event._id}
-            className='flex flex-col md:flex-row p-4 bg-white shadow-md rounded-md border border-gray-200'>
-            <div className='md:w-1/4'>
-              <img
-                src={event.mainImage}
-                alt='Event main Image'
-                className='object-cover h-full w-full'
-              />
-            </div>
-            <div className='flex flex-col md:w-3/4 p-4'>
-              <h3 className='text-lg font-semibold text-gray-800'>
-                {event.title}
-              </h3>
-              <p className='text-sm text-gray-600 mt-2 line-clamp-2'>
-                {event.description}
-              </p>
-              <div className='mt-4'>
-                <span className='text-xs text-gray-500'>
-                  {new Date(event.startDate).toLocaleDateString()} -{' '}
-                  {new Date(event.endDate).toLocaleDateString()}
-                </span>
-                <br />
-                <Link to={`/programa-eventos/${event._id}`}>
-                  <button className='mt-4 px-4 py-1 bg-gray-200 rounded-lg'>
-                    Más info
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div> {/* Fin pastEvents*/}
-
       {/* nextEvents */}
       <div className='flex flex-col gap-4 mt-4'>
-        {nextEvents.map((event) => (
+          <h2 className="text-2xl font-bold text-gray-800 my-4">Eventos próximos</h2>
+          {nextEvents.map((event) => (
           <div
             key={event._id}
-            className='flex flex-col md:flex-row p-4 bg-white shadow-md rounded-md border border-gray-200'>
+            className='flex flex-col md:flex-row p-4 bg-green-200 shadow-md rounded-md border border-gray-200'>
             <div className='md:w-1/4'>
               <img
                 src={event.mainImage}
@@ -104,6 +68,42 @@ const ProgramaEventos = () => {
           </div>
         ))}
       </div> {/* Fin nextEvents*/}
+
+      {/* pastEvents */}
+      <div className='flex flex-col gap-4 mt-4'>
+        <h2 className="text-2xl font-bold text-gray-800 my-4">Eventos pasados</h2>
+        {pastEvents.map((event) => (
+          <div
+            key={event._id}
+            className='flex flex-col md:flex-row p-4  bg-red-200 shadow-md rounded-md border border-gray-200 mb-8'>
+            <div className='md:w-1/6'>
+              <img
+                src={event.mainImage}
+                alt='Event main Image'
+                className='object-cover h-full w-full'
+              />
+            </div>
+            <div className='flex flex-col md:w-3/4 p-4'>
+              <h3 className='text-lg font-semibold text-gray-800'>
+                {event.title}
+              </h3>
+              {/* <p className='text-sm text-gray-600 mt-2 line-clamp-2'>
+                {event.description}
+              </p> */}
+              <span className='text-xs text-gray-500 mt-4'>
+                  {new Date(event.startDate).toLocaleDateString()} -{' '}
+                  {new Date(event.endDate).toLocaleDateString()}
+              </span>
+              <br />
+              <Link to={`/programa-eventos/${event._id}`}>
+                  <button className='mt-1 px-4 py-1 bg-gray-200 rounded-lg'>
+                    Más info
+                  </button>
+                </Link>
+            </div>
+          </div>
+        ))}
+      </div> {/* Fin pastEvents*/}
 
     </div>
   );
